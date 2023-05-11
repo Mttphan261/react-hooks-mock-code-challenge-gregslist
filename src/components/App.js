25e266f1e257ebdcd22cbbd135763c5146f9bca3
Matt Phan <mttphan261@gmail.com>
2023-05-10 20:48:20 -0500

    core complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  const [listingsArray, setListingsArray] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+    .then((r) => r.json())
+    .then((data) => setListingsArray(data))
+  }, [])
+
+  //CB for DELETE
+
+  function deleteListing(listing) {
+    setListingsArray([...listingsArray].filter((el) => {
+      return el.id === listing ? false : true
+    }))
+  }
+
+  //SEARCH
+
+  const [search, setSearch] = useState("")
+
+  function handleSearch(newSearch) {
+    setSearch(newSearch)
+  }
+
+  //array for searchedItems
+
+  const displayedListings = [...listingsArray].filter((el) => {
+    return el.description.toLowerCase().includes(search.toLowerCase());
+  })
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header handleSearch={handleSearch} />
+      <ListingsContainer listingsArray={displayedListings} deleteListing={deleteListing} />
     </div>
   );
 }
--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ListingCard() {
+function ListingCard({ listing, deleteListing }) {
+
+  const [fav, setFav] = useState(false)
+
+  function toggleFav() {
+    setFav((prev) => !prev)
+  }
+
+  function handleDELETE() {
+    fetch(`http://localhost:6001/listings/${listing.id}`, {
+      method: 'DELETE',
+    })
+    .then((r) => r.json())
+    .then(() => deleteListing(listing.id))
+  }
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={listing.image} alt={listing.description} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+        {fav ? (
+          <button onClick={toggleFav} className="emoji-button favorite active">★</button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button onClick={toggleFav} className="emoji-button favorite">☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+        <strong>{listing.description}</strong>
+        <span> · {listing.location}</span>
+        <button onClick={handleDELETE} className="emoji-button delete">🗑</button>
       </div>
     </li>
   );
